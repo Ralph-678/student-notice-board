2f8724775cce866c4efe80173e613ff306406ccc
Ralph-Creat <theophilejay@gmail.com>
2026-05-07 13:05:50 +1100

    add a common  for render

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.get("/api/notice", (req, res) => {
 /*
  START SERVER
 */
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
+  
